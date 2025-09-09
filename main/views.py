ffa7221d507a870428ebe1437c91f9c8dc1779f7
raihanazizah <raihana.nur41@ui.ac.id>
2025-09-09 21:21:13 +0700

    tugas 2

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def home(request):
     context = {
         'app_name': 'Peak Performance Shop',
-        'student_name': 'Raihana Nur Aziza',
+        'student_name': 'Raihana Nur Azizah',
         'student_class': 'PBP D',
     }
     return render(request, 'home.html', context)

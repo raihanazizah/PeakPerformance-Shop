6a3b6c37929fe723806847b176f6f67677bf74dd
raihanazizah <raihana.nur41@ui.ac.id>
2025-09-15 19:08:12 +0700

    tugas 3

--- main/models.py
+++ main/models.py
@@ -1,12 +1,32 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('apparel', 'Apparel'),
+        ('active_wear', 'Active Wear'),
+        ('footwear', 'Footwear'),
+        ('equipments', 'Equipments'),
+        ('nutrition_supplement', 'Nutrition & Supplement'),
+        ('accessories', 'Accessories'),
+        ('exclusive', 'Exclusive Items'),
+    ]
+    
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField(max_length=100)
+    product_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
     is_featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
+    
+    @property
+    def is_product_hot(self):
+        return self.product_views > 25
+    
+    def increment_views(self):
+        self.product_views += 1
+        self.save()
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Product
 
-# Create your tests here.
+class MainTest(TestCase): 
+    def test_main_url_is_exist(self):
+        response = Client().get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_nonexistent_page(self):
+        response = Client().get('/burhan_always_exists/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_product_creation(self):
+        product = Product.objects.create(
+            name="Sepatu Burhan",
+            price=150000,
+            description="Sepatu keramat Burhan FC",
+            thumbnail="https://example.com/sepatu.jpg",
+            category="sport",
+            is_featured=True
+        )
+        self.assertEqual(product.name, "Sepatu Burhan")
+        self.assertEqual(product.price, 150000)
+        self.assertTrue(product.is_featured)
+
+    def test_product_default_values(self):
+        product = Product.objects.create(
+            name="Kaos Panda BC",
+            price=50000,
+            description="Kaos official Panda BC",
+            thumbnail="https://example.com/kaos.jpg",
+            category="fashion"
+        )
+        # default is_featured harus False
+        self.assertFalse(product.is_featured)

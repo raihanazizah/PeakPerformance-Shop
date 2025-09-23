1a9e5ec8bbb6724ecb9b750611e48125133c2a73
raihanazizah <raihana.nur41@ui.ac.id>
2025-09-24 06:18:06 +0700

    Tugas 4 bismillah

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
@@ -11,6 +12,7 @@ class Product(models.Model):
         ('exclusive', 'Exclusive Items'),
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
